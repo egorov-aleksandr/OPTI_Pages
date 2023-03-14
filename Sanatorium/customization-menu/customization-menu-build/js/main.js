90f92df106ad9397e1bd0437b96a409543432d55
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-03-14 11:00:58 +0500

    Add files via upload

---
+++ Sanatorium/customization-menu/customization-menu-build/js/main.js
@@ -0,0 +1,36 @@
+// menu anim
+const cogwheelBtn = document.querySelector(
+	".customization-menu__button-cogwheel"
+);
+const menu = document.querySelector(".customization-menu");
+cogwheelBtn.addEventListener("click", () => {
+	if (menu.classList.contains("customization-menu-active")) {
+		menu.classList.remove("customization-menu-active");
+	} else {
+		menu.classList.add("customization-menu-active");
+	}
+});
+
+// block picker anim
+const containers = document.querySelectorAll(
+	".customization-menu__container-images"
+);
+containers.forEach((container) => {
+	const buttons = container.querySelectorAll(
+		".customization-menu__button-image"
+	);
+	let activeBtn = container.querySelector(
+		".customization-menu__button-image-active"
+	);
+	buttons.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			if (btn !== activeBtn) {
+				activeBtn.classList.remove(
+					"customization-menu__button-image-active"
+				);
+				btn.classList.add("customization-menu__button-image-active");
+				activeBtn = btn;
+			}
+		});
+	});
+});
